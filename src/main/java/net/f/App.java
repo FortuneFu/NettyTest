84c45e87902e488658d62b8f00741ea0cddc9dd0
Fortune <lachesis.f@gmail.com>
2015-04-07 16:55:16 +0800

    test

--- src/main/java/net/f/App.java
+++ src/main/java/net/f/App.java
@@ -2,12 +2,10 @@ package net.f;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        //这是一个git测试
+        System.out.println("Hello World!");
     }
 }
